b67fc2b2d925a0e38e2ffa1d01c8ebd069ca315c
Carlos Moncada <carlos.moncada@globant.com>
2018-09-06 13:51:13 -0500

    First commit
    Start project.

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoDetail :video="selectedVideo"></VideoDetail>
+    <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+const API_KEY = 'AIzaSyA9w2P6uPE0NnZIiT2VvctlPgFITGJNvBw';
+
+export default {
+  name: 'App',
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail
+  },
+  data() {
+    return { 
+      videos: [],
+      selectedVideo: null 
+    }
+  },
+  methods: {
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    },
+    onTermChange(searchTerm) {
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      }).then(response => {
+        this.videos = response.data.items
+      });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
